8c22de8c171c6d7d507b2691e8ccc4c63469b001
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-25 12:04:40 +0530

    ws

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const router = require("./routes/Studentroutes");
-const dotenv = require("dotenv"); 
+const dotenv = require("dotenv");
 
-dotenv.config(); 
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Server is running on port " + port);
+  res.send("Server is running on port " + port);
 });
 
 var cors = require("cors");
@@ -15,17 +15,19 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 const dbUrl = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
 
-mongoose.connect(dbUrl, { dbName: dbName }).then(() => {
+mongoose
+  .connect(dbUrl, { dbName: dbName })
+  .then(() => {
     console.log("Database Connected Successfully");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
